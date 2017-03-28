116a4b29c74bbfc229af76f7407218168df4b401
MarkChung90 <zhong_zhq@hisuntech.com>
2017-03-29 00:13:38 +0800

    print "Hello GitHub"

---
+++ src/com/javatest/date20170329/HelloDemo.java
@@ -0,0 +1,11 @@
+package com.javatest.date20170329;
+
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub!");
+
+	}
+
+}
