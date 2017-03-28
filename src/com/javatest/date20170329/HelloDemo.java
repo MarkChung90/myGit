bcc6e96bb40e557c0364a49f91b9c657caa099ed
MarkChung90 <zhong_zhq@hisuntech.com>
2017-03-29 00:20:40 +0800

    add static method

--- src/com/javatest/date20170329/HelloDemo.java
+++ src/com/javatest/date20170329/HelloDemo.java
@@ -1,11 +1,20 @@
 package com.javatest.date20170329;
 
 public class HelloDemo {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GitHub!");
+		
+		sayHello();
 
+	}
+
+	private static void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello myJava GitHub!");
 	}
 
 }
